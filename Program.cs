c1072f93a23cda0f53bb55b970c81bb533cb4994
Fernando <fernandoangelima@gmail.com>
2021-09-05 18:01:40 -0300

    File created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lista_de_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de Series!");
+        }
+    }
+}
